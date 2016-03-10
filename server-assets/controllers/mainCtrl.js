f58ed65466e52fc2b4aa217e00a1b766767286a1
txredking <rorymcl@gmail.com>
2016-03-09 23:58:36 -0600

    data & server

---
+++ server-assets/controllers/mainCtrl.js
@@ -0,0 +1,6 @@
+
+module.exports = {
+  getCities: function(res, res) {
+    return res.send(cities);
+  }
+}
--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 var express = require('express'),
     app = express(),
-    port = process.env.port || 7777,
+    port = process.env.PORT || 7777,
     bodyParser = require('body-parser'),
-    cors = require('cors'),
-    session = require('express-session');
+    cors = require('cors');
+    mainCtrl = require('./server-assets/controllers/mainCtrl'),
+    cities = require('./cities');
 
+    // session = require('express-session');
+
+//Show what happens without express.static on index.html with chrome tools
 app.use(bodyParser.json(), cors(), express.static(__dirname + '/public'));
+
+app.get('/api/cities', mainCtrl.getCities);
+
+
+
 
 app.listen(port, function(){
     console.log('Listening on port:', port);
